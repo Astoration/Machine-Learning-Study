a6653f532250211c160ed9607e0a4ec7f7621250
root <root@koibito.moe>
2016-08-03 13:29:57 +0900

    assign multi layer learning

--- XORLearning.py
+++ XORLearning.py
@@ -1,20 +1,24 @@
 import tensorflow as tf
 import numpy as np
 xy = np.loadtxt('trainXOR.txt',unpack=True)
-x_data = xy[0:-1]
-y_data = xy[-1]
+x_data = np.transpose(xy[0:-1])
+y_data = np.reshape(xy[-1],(4,1))
 
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
-W = tf.Variable(tf.random_uniform([1,len(x_data)],-1.0,1.0))
+W1 = tf.Variable(tf.random_uniform([2,10],-1.0,1.0))
+W2 = tf.Variable(tf.random_uniform([10,1],-1.0,1.0))
 
-h = tf.matmul(W,X)
-hypothesis = tf.div(1.,1.+tf.exp(-h))
+b1 = tf.Variable(tf.zeros([10]),name="Bias1")
+b2 = tf.Variable(tf.zeros([1]),name="Bias2")
+
+L2 = tf.sigmoid(tf.matmul(X,W1)+b1)
+hypothesis = tf.sigmoid( tf.matmul(L2,W2) + b2)
 
 cost = -tf.reduce_mean(Y*tf.log(hypothesis) + (1-Y)*tf.log(1-hypothesis))
 
-a = tf.Variable(0.01)
+a = tf.Variable(0.5)
 optimizer = tf.train.GradientDescentOptimizer(a)
 train = optimizer.minimize(cost)
 
@@ -22,10 +26,10 @@ init = tf.initialize_all_variables()
 
 with tf.Session() as sess:
 	sess.run(init)
-	for step in xrange(1000):
+	for step in xrange(8001):
 		sess.run(train, feed_dict={X:x_data, Y: y_data})
-		if step % 200 == 0:
-			print step, sess.run(cost, feed_dict={X:x_data,Y:y_data}),sess.run(W)
+		if step % 1000 == 0:
+			print step, sess.run(cost, feed_dict={X:x_data,Y:y_data}),sess.run(W1), sess.run(W2)
 	
 	correct_prediction = tf.equal(tf.floor(hypothesis+0.5), Y)
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
